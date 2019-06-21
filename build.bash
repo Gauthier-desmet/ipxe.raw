96a08c3fc8b1d621ed03be380e1989142c69b170
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-21 15:48:14 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -2,16 +2,16 @@ set -x
 
 dd if=/dev/zero of=pxeboot.img bs=1M count=4
 mkdosfs pxeboot.img
-losetup /dev/loop0 pxeboot.img
+sudo losetup /dev/loop0 pxeboot.img
 mount /dev/loop0 /mnt
 syslinux --install /dev/loop0
 wget http://boot.ipxe.org/ipxe.iso
-mount -o loop ipxe.iso /media
+sudo mount -o loop ipxe.iso /media
 cp /media/ipxe.krn /mnt
 cat > /mnt/syslinux.cfg <<EOF
 DEFAULT ipxe
 LABEL ipxe
   KERNEL ipxe.krn dhcp && chain http://ipxe.consul.local/loader.ipxe
 EOF
-umount /media
-umount /mnt
+sudo umount /media
+sudo umount /mnt

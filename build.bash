9c550032a476b36b2b4b8086ba5236777d30d092
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-21 20:04:51 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,10 +1,13 @@
 set -x
 
 wget http://boot.ipxe.org/ipxe.iso
-export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+#export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 guestfish --new pxeboot.img=fs:vfat:4M \
-          --add file://${CI_PROJECT_DIR}/ipxe.iso \
-          list-devices | tail -1
+          --add file://${CI_PROJECT_DIR}/ipxe.iso
+          --mount /dev/sda1:/mnt \
+          --mount /dev/sdb:/media \
+          list-devices | tail -1 \
+        : df -h
 
 exit 0
           --mount /dev/sda1:/mnt \

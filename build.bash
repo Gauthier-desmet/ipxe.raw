18e048b247bd03d77e7ac6598d8349ff6db2e28e
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-21 19:12:23 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,15 +1,16 @@
 set -x
 
+wget http://boot.ipxe.org/ipxe.iso
 export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 guestfish --new pxeboot.img=fs:vfat:4M \
-          --add http://boot.ipxe.org/ipxe.iso \
+          --add file://ipxe.iso \
           list-devices | tail -1
 
 exit 0
           --mount /dev/sda1:/mnt \
 guestmount -i -a pxeboot.img /mnt
 extlinux --install /mnt
-wget http://boot.ipxe.org/ipxe.iso
+
 guestmount -a ipxe.iso /media
 cp /media/ipxe.krn /mnt
 cat > /mnt/syslinux.cfg <<EOF

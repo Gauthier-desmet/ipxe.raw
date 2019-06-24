22cfcde9a1a66596956abd3a2aca39526cf1b81c
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 13:11:44 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -7,9 +7,6 @@ export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 guestfish --new ${disk_image}=fs:vfat:4M \
           --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
-part-set-mbr-id /dev/sda 1 0xb
-part-set-bootable /dev/sda 1 true
-pwrite-device /dev/sda /usr/share/syslinux/mbr.bin 0
 mount /dev/sda1 /
 mkdir /media
 mount /dev/sdb  /media
@@ -17,6 +14,8 @@ mkdir /boot
 cp /media/ipxe.krn /boot
 copy-in syslinux.cfg /boot
 extlinux /boot
+pwrite-device /dev/sda /boot/mbr.bin size:440
+part-set-bootable /dev/sda 1 true
 umount /media
 rmdir /media
 _EOF_

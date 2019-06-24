d80edcb47124e707547e0e57fbee5e4446f2225c
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 12:20:11 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -5,13 +5,9 @@ wget http://boot.ipxe.org/ipxe.iso
 guestfish --new pxeboot.img=fs:vfat:4M \
           --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
-list-devices
-list-filesystems
 mount /dev/sda1 /
 mkdir /media
 mount /dev/sdb  /media
-ls /
-ls /media
 mkdir /boot
 cp /media/ipxe.krn /boot
 copy-in syslinux.cfg /boot
@@ -19,19 +15,3 @@ extlinux /boot
 umount /media
 rmdir /media
 _EOF_
-
-exit 0
-          --mount /dev/sda1:/mnt \
-guestmount -i -a pxeboot.img /mnt
-extlinux --install /mnt
-
-guestmount -a ipxe.iso /media
-cp /media/ipxe.krn /mnt
-cat > /mnt/syslinux.cfg <<EOF
-DEFAULT ipxe
-LABEL ipxe
-  KERNEL ipxe.krn dhcp && chain http://ipxe.consul.local/loader.ipxe
-EOF
- 
-guestunmount /media
-guestunmount /mnt

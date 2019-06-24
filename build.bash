0c4205ac4d871d11628e466719513b939f25651f
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 14:30:39 +0200

    Update build.bash, .gitlab-ci.yml files

--- build.bash
+++ build.bash
@@ -4,9 +4,13 @@ disk_image="${1}"
 wget http://boot.ipxe.org/ipxe.iso
 
 export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
-guestfish --new ${disk_image}=fs:ext4:4M \
-          --add file://${CI_PROJECT_DIR}/ipxe.iso \
+guestfish --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
+run
+sparse ${disk_image} 4M
+part-disk /dev/sda gpt
+part-set-bootable /dev/sda 1 true
+mkfs ext4 /dev/sda1
 mount /dev/sda1 /
 mkdir /media
 mount /dev/sdb  /media
@@ -17,7 +21,7 @@ copy-in syslinux.cfg /boot
 extlinux /boot
 ls /boot
 pwrite-device /dev/sda /boot/mbr.bin 0
-part-set-bootable /dev/sda 1 true
+
 umount /media
 rmdir /media
 _EOF_

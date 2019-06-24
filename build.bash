685d7aee2772d2d02dce03fcd7041cc4bc377756
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 17:11:46 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -4,22 +4,24 @@ disk_image="${1}.raw"
 wget http://boot.ipxe.org/ipxe.iso
 
 export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
-qemu-img create -f raw ${disk_image} 4M
-mkdosfs ${disk_image}
-
+qemu-img create -f qcow2 ${disk_image} 1G
 guestfish --add file://${CI_PROJECT_DIR}/${disk_image} \
           --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
 run
+part-disk /dev/sda mbr
+part-set-mbr-id /dev/sda 1 0xb
+part-set-bootable /dev/sda 1 true
+pwrite-device /dev/sda /usr/share/syslinux/mbr.bin 0
+mkfs vfat /dev/sda1  
+mkmountpoint /media
+mount /dev/sda1 /
+mount /dev/sdb  /media
 list-devices
 list-filesystems
-mount /dev/sda /
-mkdir /media
-mount /dev/sdb  /media
 cp /media/ipxe.krn /
 copy-in syslinux.cfg /
-syslinux /dev/sda
+syslinux /dev/sda1
 umount /media
-rmdir /media
-
+rmmountpoint /media
 _EOF_

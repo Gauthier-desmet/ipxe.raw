0fa2ea56a53ec8da2a8aadf7903a50d62379d7d7
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 12:52:34 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -2,10 +2,13 @@ set -x
 
 disk_image="${1}"
 wget http://boot.ipxe.org/ipxe.iso
-#export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+
+export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 guestfish --new ${disk_image}=fs:vfat:4M \
           --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
+part-set-mbr-id /dev/sda 1 0xb
+part-set-bootable /dev/sda 1 true
 mount /dev/sda1 /
 mkdir /media
 mount /dev/sdb  /media

089ee6ba1d688d85bc6fc1b5b76a46a844a7316c
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-24 15:22:36 +0200

    Update build.bash, extlinux.conf files Deleted syslinux.cfg

--- build.bash
+++ build.bash
@@ -3,13 +3,13 @@ set -x
 disk_image="${1}"
 wget http://boot.ipxe.org/ipxe.iso
 
-export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
-qemu-img create -f qcow2 ${disk_image} 4M
+#export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+qemu-img create ${disk_image} 10M
 guestfish --add file://${CI_PROJECT_DIR}/${disk_image} \
           --add file://${CI_PROJECT_DIR}/ipxe.iso \
 <<_EOF_
 run
-part-disk /dev/sda gpt
+part-disk /dev/sda mbr
 part-set-bootable /dev/sda 1 true
 mkfs ext4 /dev/sda1
 mount /dev/sda1 /
@@ -18,7 +18,7 @@ mount /dev/sdb  /media
 mkdir /boot
 cp /media/ipxe.krn /boot
 copy-in /usr/share/syslinux/mbr.bin /boot
-copy-in syslinux.cfg /boot
+copy-in extlinux.conf /boot
 extlinux /boot
 ls /boot
 pwrite-device /dev/sda /boot/mbr.bin 0

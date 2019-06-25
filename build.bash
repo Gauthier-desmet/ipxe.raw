282b2d38cbc1f8eaffbe48650fcfcae76c4630a8
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-25 10:57:21 +0200

    Update build.bash, cachain.txt files

--- build.bash
+++ build.bash
@@ -1,11 +1,10 @@
 set -x
 
-CA_CERT=terena.pem
+CA_CERT=cachain.txt
 #git clone https://git.ipxe.org/ipxe.git
 git clone https://github.com/ipxe/ipxe
 cd ipxe/src
 make bin/ipxe.usb \
-     bin/undionly.kkpxe \
      TRUST=${CA_CERT} \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe
 mv bin/ipxe.usb bin/ipxe.raw

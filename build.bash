3d7139f2da58583f86278d0c896adaad60b586b2
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-25 10:40:16 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,8 +1,11 @@
 set -x
 
+CA_CERT=terena.pem
 #git clone https://git.ipxe.org/ipxe.git
 git clone https://github.com/ipxe/ipxe
 cd ipxe/src
 make bin/ipxe.usb \
+     bin/undionly.kkpxe \
+     TRUST=${CA_CERT} \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe
 mv bin/ipxe.usb bin/ipxe.raw

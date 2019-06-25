6befa38324388a045085bbc6256b5e0d4e79106a
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-25 10:18:02 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,6 +1,7 @@
 set -x
 
-git clone https://git.ipxe.org/ipxe.git
+#git clone https://git.ipxe.org/ipxe.git
+git clone https://github.com/ipxe/ipxe
 cd ipxe/src
 make bin/ipxe.usb \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe

393945ac9d7190f033809f0b8304af05e434a426
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-25 11:20:08 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -4,7 +4,7 @@ CA_CERT=cachain.txt
 #git clone https://git.ipxe.org/ipxe.git
 git clone https://github.com/ipxe/ipxe
 cd ipxe/src
-printf '‰s\n' '#define DOWNLOAD_PROTO_HTTPS /* Secure Hypertext Transfer Protocol */' >> config/general.h
+printf '%s\n' '#define DOWNLOAD_PROTO_HTTPS /* Secure Hypertext Transfer Protocol */' >> config/general.h
 make bin/ipxe.usb \
      TRUST=${CI_PROJECT_DIR}/${CA_CERT} \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe

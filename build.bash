893be92fef4046831d1357f837706b3e25f0ed34
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-26 14:55:24 +0200

    Update embedded.ipxe, build.bash files

--- build.bash
+++ build.bash
@@ -10,7 +10,8 @@ printf '%s\n' '#define DOWNLOAD_PROTO_HTTPS /* Secure Hypertext Transfer Protoco
 printf '%s\n' '#define PING_CMD /* Ping command */' >> config/general.h
 printf '%s\n' '#define NSLOOKUP_CMD /* Name resolution command */' >> config/general.h
 printf '%s\n' '#define NEIGHBOUR_CMD /* Neighbour management commands */' >> config/general.h
-sed -i -e "s/KEYBOARD_MAP us/KEYBOARD_MAP fr/g" config/general.h
+sed -i '/KEYBOARD_MAP/d' config/general.h
+printf '%s\n' '#define KEYBOARD_MAP fr' >> config/general.h
 
 make bin/ipxe.usb \
      TRUST=${CI_PROJECT_DIR}/${CA_CERT} \

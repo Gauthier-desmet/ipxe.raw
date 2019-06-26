8ef510bae096555825c54a26e1707b210d748bf9
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-26 16:38:18 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -10,11 +10,17 @@ printf '%s\n' '#define DOWNLOAD_PROTO_HTTPS /* Secure Hypertext Transfer Protoco
 printf '%s\n' '#define PING_CMD /* Ping command */' >> config/general.h
 printf '%s\n' '#define NSLOOKUP_CMD /* Name resolution command */' >> config/general.h
 printf '%s\n' '#define NEIGHBOUR_CMD /* Neighbour management commands */' >> config/general.h
-sed -i '/KEYBOARD_MAP/d' config/general.h
-printf '%s\n' '#define KEYBOARD_MAP fr' >> config/general.h
+#sed -i '/KEYBOARD_MAP/d' config/general.h
+#printf '%s\n' '#define KEYBOARD_MAP fr' >> config/general.h
 
 make bin/ipxe.usb \
      TRUST=${CI_PROJECT_DIR}/${CA_CERT} \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe
 
 mv bin/ipxe.usb ${CI_PROJECT_DIR}/ipxe.raw
+
+make bin/ipxe.iso \
+     TRUST=${CI_PROJECT_DIR}/${CA_CERT} \
+     EMBED=${CI_PROJECT_DIR}/embedded.ipxe
+
+mv bin/ipxe.iso ${CI_PROJECT_DIR}/ipxe.iso

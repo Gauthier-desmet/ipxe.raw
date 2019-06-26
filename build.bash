9407c288fbcd93cb9ec8f77d41700700ab142e8d
David DELAVENNAT <david.delavennat@polytechnique.edu>
2019-06-26 18:32:09 +0200

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,10 +1,17 @@
 set -x
 
-CA_CERT=AddTrust_External_CA_Root.pem
-CA_CERT_URL=http://www.terena.org/activities/tcs/repository/${CA_CERT}
-curl --location \
-     --output ${CA_CERT} \
-     ${CA_CERT_URL}
+CA_CERT1=AddTrust_External_CA_Root.pem
+CA_CERT_URL1=http://www.terena.org/activities/tcs/repository/${CA_CERT1}
+
+CA_CERT2=USERTrustRSAAddTrustCA.crt
+CA_CERT_URL2=http://crt.usertrust.com/${CA_CERT2}
+
+CA_CERT3=TERENASSLCA2.crt
+CA_CERT_URL3=http://crt.usertrust.com/${CA_CERT3}
+
+curl --location --output ${CA_CERT1} ${CA_CERT_URL1}
+curl --location --output ${CA_CERT2} ${CA_CERT_URL2}
+curl --location --output ${CA_CERT3} ${CA_CERT_URL3}
 
 #git clone https://git.ipxe.org/ipxe.git
 git clone https://github.com/ipxe/ipxe
@@ -19,16 +26,9 @@ printf '%s\n' '#define NEIGHBOUR_CMD /* Neighbour management commands */' >> con
 #printf '%s\n' '#define KEYBOARD_MAP fr' >> config/general.h
 
 make bin/ipxe.usb \
-     CERT=${CI_PROJECT_DIR}/${CA_CERT} \
-     TRUST=${CI_PROJECT_DIR}/${CA_CERT} \
+     CERT=${CI_PROJECT_DIR}/${CA_CERT1},${CI_PROJECT_DIR}/${CA_CERT2},${CI_PROJECT_DIR}/${CA_CERT3} \
+     TRUST=${CI_PROJECT_DIR}/${CA_CERT1} \
      EMBED=${CI_PROJECT_DIR}/embedded.ipxe \
      DEBUG=tls,x509,httpcore,https,rootcert
 
 mv bin/ipxe.usb ${CI_PROJECT_DIR}/ipxe.raw
-
-#make bin/ipxe.iso \
-#     TRUST=${CI_PROJECT_DIR}/${CA_CERT} \
-#     EMBED=${CI_PROJECT_DIR}/embedded.ipxe \
-#     DEBUG=x509,httpcore,https,rootcert
-
-#mv bin/ipxe.iso ${CI_PROJECT_DIR}/ipxe.iso
